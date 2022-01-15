40e7602437ba6af0ed59f4ea4850f0eddeaced3b
khanhduy1407 <kn145660@gmail.com>
2022-01-15 20:21:41 +0700

    Construct the first game loop (sleep)

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -41,10 +41,28 @@ public class GamePanel extends JPanel implements Runnable {
 
     @Override
     public void run() {
+        double drawInterval = 1000000000 / FPS; // ≈ 0.0167 seconds
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
         while (gameThread != null) {
             update();
 
             repaint();
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime /= 1000000;
+
+                if (remainingTime < 0) {
+                    remainingTime = 0;
+                }
+
+                Thread.sleep((long) remainingTime);
+
+                nextDrawTime += drawInterval;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 

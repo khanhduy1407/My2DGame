b04f8f6c934109dbf401a3b2a5015376d9695da7
khanhduy1407 <kn145660@gmail.com>
2022-01-29 19:55:27 +0700

    Draw objects

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -92,8 +92,17 @@ public class GamePanel extends JPanel implements Runnable {
 
         Graphics2D g2 = (Graphics2D) g;
 
+        // TILE
         tileM.draw(g2);
 
+        // OBJECT
+        for (int i = 0; i < obj.length; i++) {
+            if (obj[i] != null) {
+                obj[i].draw(g2, this);
+            }
+        }
+
+        // PLAYER
         player.draw(g2);
 
         g2.dispose();

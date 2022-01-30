86ad00cd0440d0d6ee2f62b283dc3901c551b114
khanhduy1407 <kn145660@gmail.com>
2022-01-30 17:35:29 +0700

    Draw NPC

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -127,6 +127,13 @@ public class GamePanel extends JPanel implements Runnable {
             }
         }
 
+        // NPC
+        for (int i = 0; i < npc.length; i++) {
+            if (npc[i] != null) {
+                npc[i].draw(g2);
+            }
+        }
+
         // PLAYER
         player.draw(g2);
 
--- src/object/SuperObject.java
+++ src/object/SuperObject.java
@@ -18,7 +18,6 @@ public class SuperObject {
     UtilityTool uTool = new UtilityTool();
 
     public void draw(Graphics2D g2, GamePanel gp) {
-
         int screenX = worldX - gp.player.worldX + gp.player.screenX;
         int screenY = worldY - gp.player.worldY + gp.player.screenY;
 

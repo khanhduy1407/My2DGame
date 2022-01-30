90ce19eb26bfa21a2d51d0900e5276ee418d7878
khanhduy1407 <kn145660@gmail.com>
2022-01-30 08:13:20 +0700

    Show Notification

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -108,7 +108,7 @@ public class Player extends Entity {
                     gp.playSE(1);
                     hasKey++;
                     gp.obj[i] = null;
-                    System.out.println("Key: " + hasKey);
+                    gp.ui.showMessage("You got a key!");
                     break;
                 case "Door":
                     if (hasKey > 0) {
@@ -116,7 +116,6 @@ public class Player extends Entity {
                         gp.obj[i] = null;
                         hasKey--;
                     }
-                    System.out.println("Key: " + hasKey);
                     break;
                 case "Boots":
                     gp.playSE(2);

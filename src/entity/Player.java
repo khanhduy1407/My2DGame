139882fe58a320b6de138effe44193b237d6b6bc
khanhduy1407 <kn145660@gmail.com>
2022-01-30 20:45:45 +0700

    Fix bug collision #2

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -15,8 +15,6 @@ public class Player extends Entity {
     public final int screenX;
     public final int screenY;
     int standCounter = 0;
-    boolean moving = false;
-    int pixelCounter = 0;
 
     public Player(GamePanel gp, KeyHandler keyH) {
         super(gp);
@@ -27,12 +25,12 @@ public class Player extends Entity {
         screenY = gp.screenHeight / 2 - (gp.tileSize / 2);
 
         solidArea = new Rectangle();
-        solidArea.x = 1;
-        solidArea.y = 1;
+        solidArea.x = 8;
+        solidArea.y = 16;
         solidAreaDefaultX = solidArea.x;
         solidAreaDefaultY = solidArea.y;
-        solidArea.width = 46;
-        solidArea.height = 46;
+        solidArea.width = 32;
+        solidArea.height = 32;
 
         setDefaultValues();
         getPlayerImage();
@@ -57,41 +55,29 @@ public class Player extends Entity {
     }
 
     public void update() {
-        if (!moving) {
-            if (keyH.upPressed || keyH.downPressed || keyH.leftPressed || keyH.rightPressed) {
-                if (keyH.upPressed) {
-                    direction = "up";
-                } else if (keyH.downPressed) {
-                    direction = "down";
-                } else if (keyH.leftPressed) {
-                    direction = "left";
-                } else if (keyH.rightPressed) {
-                    direction = "right";
-                }
-
-                moving = true;
-
-                // CHECK TILE COLLISION
-                collisionOn = false;
-                gp.cChecker.checkTile(this);
-
-                // CHECK OBJECT COLLISION
-                int objIndex = gp.cChecker.checkObject(this, true);
-                pickUpObject(objIndex);
-
-                // CHECK NPC COLLISION
-                int npcIndex = gp.cChecker.checkEntity(this, gp.npc);
-                interactNPC(npcIndex);
-            } else {
-                standCounter++;
-
-                if (standCounter == 20) {
-                    spriteNum = 1;
-                    standCounter = 0;
-                }
+        if (keyH.upPressed || keyH.downPressed || keyH.leftPressed || keyH.rightPressed) {
+            if (keyH.upPressed) {
+                direction = "up";
+            } else if (keyH.downPressed) {
+                direction = "down";
+            } else if (keyH.leftPressed) {
+                direction = "left";
+            } else if (keyH.rightPressed) {
+                direction = "right";
             }
-        }
-        if (moving) {
+
+            // CHECK TILE COLLISION
+            collisionOn = false;
+            gp.cChecker.checkTile(this);
+
+            // CHECK OBJECT COLLISION
+            int objIndex = gp.cChecker.checkObject(this, true);
+            pickUpObject(objIndex);
+
+            // CHECK NPC COLLISION
+            int npcIndex = gp.cChecker.checkEntity(this, gp.npc);
+            interactNPC(npcIndex);
+
             // IF COLLISION IS FALSE, PLAYER CAN MOVE
             if (!collisionOn) {
                 switch (direction) {
@@ -111,12 +97,12 @@ public class Player extends Entity {
                 }
                 spriteCounter = 0;
             }
+        } else {
+            standCounter++;
 
-            pixelCounter += speed;
-
-            if (pixelCounter == 48) {
-                moving = false;
-                pixelCounter = 0;
+            if (standCounter == 20) {
+                spriteNum = 1;
+                standCounter = 0;
             }
         }
     }

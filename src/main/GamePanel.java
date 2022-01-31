d2535e5f2a5aa0227453559db60dc88d51009764
khanhduy1407 <kn145660@gmail.com>
2022-01-31 20:33:38 +0700

    Death animation

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -120,7 +120,12 @@ public class GamePanel extends JPanel implements Runnable {
             // MONSTER
             for (int i = 0; i < monster.length; i++) {
                 if (monster[i] != null) {
-                    monster[i].update();
+                    if (monster[i].alive && !monster[i].dying) {
+                        monster[i].update();
+                    }
+                    if (!monster[i].alive) {
+                        monster[i] = null;
+                    }
                 }
             }
         }

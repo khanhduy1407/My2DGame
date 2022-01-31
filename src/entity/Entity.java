afd0784976292ce63d9b9539955b1d3bf348929a
khanhduy1407 <kn145660@gmail.com>
2022-01-31 19:15:13 +0700

    Preparing attack image

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -14,6 +14,7 @@ public class Entity {
     public int worldX, worldY;
     public int speed;
     public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+    public BufferedImage attackUp1, attackUp2, attackDown1, attackDown2, attackLeft1, attackLeft2, attackRight1, attackRight2;
     public String direction = "down";
     public int spriteCounter = 0;
     public int spriteNum = 1;

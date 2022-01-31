25d1c04ea88bcfdc68d21c3de6c9b60bfdf52c7b
khanhduy1407 <kn145660@gmail.com>
2022-01-31 18:58:46 +0700

    Monster attacking player

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -28,6 +28,7 @@ public class Entity {
     public BufferedImage image, image2, image3;
     public String name;
     public boolean collision = false;
+    public int type; // 0 = player, 1 = npc, 2 = monster
 
     // CHARACTER STATUS
     public int maxLife;
@@ -70,7 +71,15 @@ public class Entity {
         gp.cChecker.checkObject(this, false);
         gp.cChecker.checkEntity(this, gp.npc);
         gp.cChecker.checkEntity(this, gp.monster);
-        gp.cChecker.checkPlayer(this);
+        boolean contactPlayer = gp.cChecker.checkPlayer(this);
+
+        if (this.type == 2 && contactPlayer) {
+            if (!gp.player.invincible) {
+                // we can give damage
+                gp.player.life -= 1;
+                gp.player.invincible = true;
+            }
+        }
 
         // IF COLLISION IS FALSE, PLAYER CAN MOVE
         if (!collisionOn) {

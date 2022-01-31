b14bab9519c1c9f7413d5f8da3bd86e455d6e944
khanhduy1407 <kn145660@gmail.com>
2022-01-31 10:54:14 +0700

    Replace Rectangle with EventRect

--- src/main/EventHandler.java
+++ src/main/EventHandler.java
@@ -22,6 +22,12 @@ public class EventHandler {
             eventRect[col][row].height = 2;
             eventRect[col][row].eventRectDefaultX = eventRect[col][row].x;
             eventRect[col][row].eventRectDefaultY = eventRect[col][row].y;
+
+            col++;
+            if (col == gp.maxWorldCol) {
+                col = 0;
+                row++;
+            }
         }
     }
 
@@ -30,15 +36,15 @@ public class EventHandler {
         if (hit(23, 12, "up")) {healingPool(gp.dialogueState);}
     }
 
-    public boolean hit(int eventCol, int eventRow, String reqDirection) {
+    public boolean hit(int col, int row, String reqDirection) {
         boolean hit = false;
 
         gp.player.solidArea.x = gp.player.worldX + gp.player.solidArea.x;
         gp.player.solidArea.y = gp.player.worldY + gp.player.solidArea.y;
-        eventRect.x = eventCol * gp.tileSize + eventRect.x;
-        eventRect.y = eventRow * gp.tileSize + eventRect.y;
+        eventRect[col][row].x = col * gp.tileSize + eventRect[col][row].x;
+        eventRect[col][row].y = row * gp.tileSize + eventRect[col][row].y;
 
-        if (gp.player.solidArea.intersects(eventRect)) {
+        if (gp.player.solidArea.intersects(eventRect[col][row])) {
             if (gp.player.direction.contentEquals(reqDirection) || reqDirection.contentEquals("any")) {
                 hit = true;
             }
@@ -46,8 +52,8 @@ public class EventHandler {
 
         gp.player.solidArea.x = gp.player.solidAreaDefaultX;
         gp.player.solidArea.y = gp.player.solidAreaDefaultY;
-        eventRect.x = eventRectDefaultX;
-        eventRect.y = eventRectDefaultY;
+        eventRect[col][row].x = eventRect[col][row].eventRectDefaultX;
+        eventRect[col][row].y = eventRect[col][row].eventRectDefaultY;
 
         return hit;
     }

ef5eec2d2c37028626ee4eb813b55f22ef24fb45
khanhduy1407 <kn145660@gmail.com>
2022-02-01 13:36:48 +0700

    Draw Description Text

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -75,15 +75,6 @@ public class Player extends Entity {
         inventory.add(currentWeapon);
         inventory.add(currentShield);
         inventory.add(new OBJ_Key(gp));
-        inventory.add(new OBJ_Key(gp));
-        inventory.add(new OBJ_Key(gp));
-        inventory.add(new OBJ_Key(gp));
-        inventory.add(new OBJ_Key(gp));
-        inventory.add(new OBJ_Key(gp));
-        inventory.add(new OBJ_Key(gp));
-        inventory.add(new OBJ_Key(gp));
-        inventory.add(new OBJ_Key(gp));
-        inventory.add(new OBJ_Key(gp));
     }
 
     private int getAttack() {
